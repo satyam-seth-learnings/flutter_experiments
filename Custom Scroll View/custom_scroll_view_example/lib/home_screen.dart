30ab7800366bcc80316553576339b4dfac2d3074
satyam-seth <satyam1998.1998@gmail.com>
2023-02-21 00:48:21 +0530

    add custom scroll view example

---
+++ Custom Scroll View/custom_scroll_view_example/lib/home_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'widgets/about_widget.dart';
+import 'widgets/header_widget.dart';
+import 'widgets/tab_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              pinned: true,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.grey,
+                ),
+              ),
+              backgroundColor: Colors.black,
+              elevation: 0,
+              systemOverlayStyle: SystemUiOverlayStyle.dark,
+              actions: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.help_outline_rounded,
+                    color: Colors.grey,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.more_vert,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+            const SliverToBoxAdapter(
+              child: HeaderWidget(),
+            ),
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: TabWidget(),
+            ),
+            const SliverToBoxAdapter(
+              child: AboutWidget(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
