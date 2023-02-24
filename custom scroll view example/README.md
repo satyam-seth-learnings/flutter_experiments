bdf477deac1716aa57aa5ddfce466570945a0f64
satyam-seth <satyam1998.1998@gmail.com>
2023-02-25 00:27:41 +0530

    add example 2

--- custom scroll view example/README.md
+++ custom scroll view example/README.md
@@ -1,1 +1,11 @@
-[Youtube Videos Link](https://youtu.be/iCEy8QfFLyI)
+[Doc Link](https://api.flutter.dev/flutter/widgets/CustomScrollView-class.html)
+
+# First Example -
+
+- [Youtube Videos Link](https://youtu.be/iCEy8QfFLyI)
+
+
+# Second Example -
+
+- [Blog Link](https://blog.logrocket.com/building-custom-flutter-scrollview/)
+
--- custom scroll view example/custom_scroll_view_example/lib/screens/home_screen.dart
+++ custom scroll view example/custom_scroll_view_example/lib/screens/home_screen.dart
@@ -1,20 +1,40 @@
 import 'package:flutter/material.dart';
 
 import 'first_example.dart';
+import 'second_example.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  ElevatedButton _firstExampleButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const FirstExample(),
-          ),
-        );
-      },
-      child: const Text('First Example'),
+  Widget _firstExampleButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const FirstExample(),
+            ),
+          );
+        },
+        child: const Text('First Example'),
+      ),
+    );
+  }
+
+  Widget _secondExampleButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const SecondExample(),
+            ),
+          );
+        },
+        child: const Text('Second Example'),
+      ),
     );
   }
 
@@ -28,6 +48,7 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               _firstExampleButton(context),
+              _secondExampleButton(context),
             ],
           ),
         ),
