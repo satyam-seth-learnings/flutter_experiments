0521a41fb80d3b63cde23ea463e450843104cd16
satyam-seth <satyam1998.1998@gmail.com>
2023-06-03 01:22:39 +0530

    complete first part of Architect your Flutter project using BLOC pattern

---
+++ Architect your Flutter project using BLOC pattern/README.md
@@ -0,0 +1,9 @@
+[Architect your flutter project using BLOC pattern part 1 blog](https://medium.com/codechai/architecting-your-flutter-project-bd04e144a8f1)
+
+[Architect your flutter project using BLOC pattern part 2 blog](https://medium.com/flutterpub/architect-your-flutter-project-using-bloc-pattern-part-2-d8dd1eca9ba5)
+
+[rxdart Package](https://pub.dev/packages/rxdart)
+
+[http Package](https://pub.dev/packages/http)
+
+[TMDB Api]
---
+++ Architect your Flutter project using BLOC pattern/architect_project_using_bloc_part1/lib/src/ui/movie_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../blocs/movie_bloc.dart';
+import '../models/item_model.dart';
+
+class MovieList extends StatefulWidget {
+  const MovieList({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MovieListState();
+  }
+}
+
+class MovieListState extends State<MovieList> {
+  @override
+  void initState() {
+    super.initState();
+    bloc.fetchAllMovies();
+  }
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Popular Movies'),
+      ),
+      body: StreamBuilder(
+        stream: bloc.allMovies,
+        builder: (context, AsyncSnapshot<ItemModel> snapshot) {
+          if (snapshot.hasData) {
+            return buildList(snapshot);
+          } else if (snapshot.hasError) {
+            return Text(snapshot.error.toString());
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+
+  Widget buildList(AsyncSnapshot<ItemModel> snapshot) {
+    return GridView.builder(
+        itemCount: snapshot.data!.results!.length,
+        gridDelegate:
+            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return GridTile(
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w185${snapshot.data!.results![index].posterPath!}',
+              fit: BoxFit.cover,
+            ),
+          );
+        });
+  }
+}
